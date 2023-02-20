a8a6ce24cb29575449f5a885ae21f5d3e8f0e1c1
MolenR <remcovdmolen@hotmail.com>
2023-02-20 15:23:34 +0100

    API testing swagger & Postman. Seq still not tracking in dash. Serilog Ok

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,6 +1,8 @@
 using HotelListing.API.Data;
+using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +34,14 @@ builder.Services.AddCors(options =>
 /* LOGGING SERILOG & SEQ 
 --------------------------------------*/
 builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+/*Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341")
+    .CreateLogger();*/
+
+Serilog.Debugging.SelfLog.Enable(Console.Error);
+
 
 var app = builder.Build();
 
