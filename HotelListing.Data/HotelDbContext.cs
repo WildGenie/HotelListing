8798bba9b422355a74b91879d2e05e7be356d093
MolenR <remcovdmolen@hotmail.com>
2023-02-27 11:39:15 +0100

    ///---MAJOR SCAFFOLD ISSUES RESOLVED---/// Add Factory

--- HotelListing.Data/HotelDbContext.cs
+++ HotelListing.Data/HotelDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using HotelListing.API.Configurations.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace HotelListing.API.Data;
 
@@ -24,4 +26,23 @@ public class HotelDbContext : IdentityDbContext<User>
 
     public DbSet<Hotel> Hotels { get; set; }
     public DbSet<Country> Countries { get; set; }
+}
+
+/* ADDING DBCONTEXT FACTORY SCAFFOLDING ISSUES ARE RESOLVED
+---------------------------------------------------------*/
+public class HotelDbContextFactory : IDesignTimeDbContextFactory<HotelDbContext>
+{
+    public HotelDbContext CreateDbContext(string[] args)
+    {
+        IConfiguration config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<HotelDbContext>();
+        var connect = config.GetConnectionString("HotelAPIDbString");
+        
+        optionsBuilder.UseSqlServer(connect);
+
+        return new HotelDbContext(optionsBuilder.Options);
+    }
 }
